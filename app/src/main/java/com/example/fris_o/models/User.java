ec4b1cc5a5c9c669f92fe6ffaf76ff1b2ac7eaed
adriand98 <dumitrache.g.98@gmail.com>
2020-02-18 17:20:27 +0000

    Revert "AI-3.5.3 <muhsindbt@Muhsins-MacBook-Pro.local Overwrite remote https://github.com/BrunelCS/group-project-2019-20-group-20.git to local"
    This reverts commit 6ad38b7b189c0718f8b319a10d4fbdfc3b9e72ea.

---
+++ app/src/main/java/com/example/fris_o/models/User.java
@@ -0,0 +1,42 @@
+package com.example.fris_o.models;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
